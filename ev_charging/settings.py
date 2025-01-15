17b657d71700c5bd6a64d0f1c95855dbd3e357c3
se-thato <thatoselepe80@gmail.com>
2025-01-15 13:31:02 +0000

    made changes

--- ev_charging/settings.py
+++ ev_charging/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 
     'charging_station',
     'VoltHub',
@@ -82,7 +83,7 @@ ROOT_URLCONF = 'ev_charging.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
