d899a28e855fcea0fe41841075f1fa7e86611c6e
se-thato <thatoselepe80@gmail.com>
2025-09-09 08:44:52 +0200

    footer changes

--- ev_charging/settings.py
+++ ev_charging/settings.py
@@ -238,12 +238,8 @@ if DB_LIVE:
     }
 else:
     
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+"""
 """
 DATABASES = {
     'default': {
@@ -255,7 +251,13 @@ DATABASES = {
         'PORT': os.environ.get("DB_PORT"),
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
