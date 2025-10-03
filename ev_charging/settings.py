ddbe3432bef0a62b43dbce4b0600f2a54d5a7e38
se-thato <thatoselepe80@gmail.com>
2025-10-03 17:42:14 +0200

    add to cart function

--- ev_charging/settings.py
+++ ev_charging/settings.py
@@ -248,7 +248,6 @@ if DB_LIVE:
         }
     }
 else:
-    
 
 """
 
@@ -263,14 +262,14 @@ DATABASES = {
     }
 }
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
