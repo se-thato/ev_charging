37fd59bd307adf8ff8de6f8d0ed2983a9f4df8a0
se-thato <thatoselepe80@gmail.com>
2025-10-28 02:18:09 +0200

    views changes

--- authentication/views.py
+++ authentication/views.py
@@ -62,8 +62,8 @@ def register(request):
         if form.is_valid():
             user = form.save(commit=False)  # Don't save yet
             user.is_active = False  # Deactivate account till it is confirmed
-            user.save()  # Now we will save the user
-            activateEmail(request, user, form.cleaned_data.get('email'))
+            #user.save()  # Now we will save the user
+            #activateEmail(request, user, form.cleaned_data.get('email'))
 
             
 
