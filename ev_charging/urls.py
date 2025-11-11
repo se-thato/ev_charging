8a329d8a2c7243eedf4f5bc8b6647ec2d3c6c126
se-thato <thatoselepe80@gmail.com>
2025-11-11 15:44:35 +0200

    fix DEBUG issue

--- ev_charging/urls.py
+++ ev_charging/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-import os
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -45,6 +44,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-if settings.DEBUG or os.getenv("SERVE_MEDIA", "False") == "True":
+if settings.DEBUG:
    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
