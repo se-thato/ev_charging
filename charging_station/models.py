1494c6213da7c74e6808760474b8339eb8321a4b
se-thato <thatoselepe80@gmail.com>
2025-11-18 16:25:52 +0200

    made media changes

--- charging_station/models.py
+++ charging_station/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100, null=True, blank=True)
     location = models.CharField(max_length=150, null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='media/profile_pictures/', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
 
     def __str__(self):
         return self.username
