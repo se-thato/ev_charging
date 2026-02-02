24f2c8d5502d737d3feccf9dfe00b6e54611b58e
se-thato <thatoselepe80@gmail.com>
2026-02-02 22:13:23 +0200

    account activate

--- authentication/views.py
+++ authentication/views.py
@@ -47,64 +47,37 @@ def activate(request, uidb64, token):
         return redirect('authentication:login')
 
 
-# def activateEmail(request, user, to_email):
+def activateEmail(request, user, to_email):
 
-#     if settings.DEBUG:
-#         domain = get_current_site(request).domain 
-#     else:
-#         domain = "evcharging-production-c179.up.railway.app" 
+    if settings.DEBUG:
+        domain = get_current_site(request).domain 
+    else:
+        domain = "evcharging-production-c179.up.railway.app" 
 
-#     mail_subject = "Activate your account"
+    mail_subject = "Activate your account"
     
-#     message = render_to_string("authentication/activate_account.html", {
-#         "user": user.username,
-#         "domain": domain,
-#         "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-#         "token": account_activation_token.make_token(user),
-#         "protocol": "https" if request.is_secure() else "http",
-#     })
-#     email = EmailMessage(mail_subject, message, to=[to_email])
+    message = render_to_string("authentication/activate_account.html", {
+        "user": user.username,
+        "domain": domain,
+        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+        "token": account_activation_token.make_token(user),
+        "protocol": "https" if request.is_secure() else "http",
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
 
-#     email.content_subtype = "html"
+    email.content_subtype = "html"
 
-#     if email.send():
-#         messages.success(request, f'Dear {user.username}, please go to your email {to_email} inbox and click the verification link to verify your account!Note: If you don\'t see the email, please check your spam folder.')
-#     else:
-#         messages.error(request, f'Dear {user.username}, there was an error sending the verification email. Please try again later.')
-
-# def activateEmail(request, user, to_email):
-#     from django.contrib.sites.shortcuts import get_current_site
-
-#     # Get domain from Sites framework if available
-#     current_site = get_current_site(request)
-#     domain = current_site.domain if current_site else "localhost:8000"
-
-#     # Use https in production
-#     protocol = "https" if not settings.DEBUG else "http"
-
-#     mail_subject = "Activate your account"
-    
-#     message = render_to_string("authentication/activate_account.html", {
-#         "user": user.username,
-#         "domain": domain,
-#         "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-#         "token": account_activation_token.make_token(user),
-#         "protocol": protocol,
-#     })
-#     email = EmailMessage(mail_subject, message, to=[to_email])
-#     email.content_subtype = "html"
-
-#     if email.send():
-#         messages.success(request, f'Dear {user.username}, please check your email {to_email} to verify your account!')
-#     else:
-#         messages.error(request, f'Dear {user.username}, there was an error sending the verification email.')
+    if email.send():
+        messages.success(request, f'Dear {user.username}, please go to your email {to_email} inbox and click the verification link to verify your account!Note: If you don\'t see the email, please check your spam folder.')
+    else:
+        messages.error(request, f'Dear {user.username}, there was an error sending the verification email. Please try again later.')
 
 
-# def activation_success(request):
-#     return render(request, 'authentication/activation_success.html')
+def activation_success(request):
+    return render(request, 'authentication/activation_success.html')
 
-# def activation_failed(request):
-#     return render(request, 'authentication/activation_failed.html')
+def activation_failed(request):
+    return render(request, 'authentication/activation_failed.html')
 
 def register(request):
     form = UserRegisterForm()
@@ -123,7 +96,7 @@ def register(request):
             user = form.save(commit=False)  # Don't save yet
             user.is_active = False  # Deactivate account till it is confirmed
             user.save()  # Now we will save the user
-            #activateEmail(request, user, form.cleaned_data.get('email'))  # Send activation email
+            activateEmail(request, user, form.cleaned_data.get('email'))  # Send activation email
             messages.success(request, f"Activation email sent to {form.cleaned_data.get('email')}.")
 
             # Create Profile only after the user has been successfully created
