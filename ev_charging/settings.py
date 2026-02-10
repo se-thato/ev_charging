1965d0775bc178444894f09ce20a3a171aea0d0e
se-thato <thatoselepe80@gmail.com>
2026-02-10 10:55:24 +0200

    connection fixes

--- ev_charging/settings.py
+++ ev_charging/settings.py
@@ -278,11 +278,11 @@ CHANNEL_LAYERS = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.environ.get("DB_NAME", ""),
+        "USER": os.environ.get("DB_USER", ""),
+        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("DB_PORT", "3306"),
         "OPTIONS": {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
