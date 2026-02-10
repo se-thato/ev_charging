b117c34d418d93bfa60899309d8b3f9de6aa0c1e
se-thato <thatoselepe80@gmail.com>
2026-02-10 11:13:03 +0200

    connection fixes

--- ev_charging/settings.py
+++ ev_charging/settings.py
@@ -281,7 +281,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME", ""),
         "USER": os.environ.get("DB_USER", ""),
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
-        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "HOST": os.environ.get("DB_HOST", "mainline.proxy.rlwy.net"),
         "PORT": os.environ.get("DB_PORT", "3306"),
         "OPTIONS": {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
