a83532e25bd8fddd479406406dcefcb732ecf19d
se-thato <thatoselepe80@gmail.com>
2026-03-26 13:14:12 +0200

    fix:modify statifiles

--- ev_charging/settings.py
+++ ev_charging/settings.py
@@ -355,7 +355,6 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 #STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
     BASE_DIR / "VoltHub" / "static",
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
